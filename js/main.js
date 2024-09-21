86560cba285fdb2fe96ed1415bebfb2da116ab8c
Agustin-Sosa <agustinomarsosa@gmail.com>
2024-09-21 20:32:06 -0300

    avance para la entrega final

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
+let catalogo = [];
+let carrito = [];
 
-class Producto{
-    constructor(nombre, peso, precio, img){
-        this.nombre = nombre;
-        this.peso = peso;
-        this.precio = precio;
-        this.img = img;
-    }
-}
-
-const producto1 = new Producto ("Leña", "Bolsa de 6kg", parseInt(1500), "assets/img/bolsaAsado.jpeg");
-const producto2 = new Producto ("Carbón", "Bolsa de 3kg", parseInt(1500), "assets/img/carbon1.jpg");
-const producto3 = new Producto ("Leña", "Bolsa de 15kg", parseInt(3500), "assets/img/descarga.jpg");
-const producto4 = new Producto ("Leña", "suelta por kg", parseInt(200), "assets/img/leña-ASADO.jpeg");
-
-
-
-let carrito = {
-    producto1: { ...producto1, cantidad: 0 },
-    producto2: { ...producto2, cantidad: 0 },
-    producto3: { ...producto3, cantidad: 0 },
-    producto4: { ...producto4, cantidad: 0 }
-};
-
-let catalogo = [producto1, producto2, producto3, producto4];
+fetch('./data/productos.json')
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Error al cargar el catálogo');
+        } 
+        return response.json();
+    })
+    .then((data) =>{
+        catalogo = data;
+        carrito = catalogo.map(producto => ({ ...producto, cantidad: 0 }));
+        crearHTML(catalogo)
+        cargarCarritoDesdeLocalStorage();
+    })
+    .catch((error) => {
+        console.error("Hubo un problema con el fetch:", error);
+        Swal.fire({
+            title: 'Error',
+            text: 'No se pudo cargar el catálogo de productos.',
+            icon: 'error',
+        });
+    });
+    
 
 const verCarrito = document.getElementById("btn-carrito");
 
@@ -42,17 +42,112 @@ verCarrito.addEventListener('click', () => {
 
 function mostrarCarrito(obj) {
     let html = '';
+    let hayProductos = false;
     for (const key in obj) {
         const producto = obj[key];
-        html += 
+        if(producto.cantidad > 0){
+            hayProductos = true;
+            html += 
         `<div class="">
             <h2>Cantidad de ${producto.nombre} (${producto.peso}): ${producto.cantidad}</h2>
         </div>`;
+        }
+    }
+    if(!hayProductos){
+        html = `<div><h2>El carrito está vacío</h2></div>`;
+    } else {
+        html += 
+        `<div>
+            <button id="btn-delete">Eliminar producto del carrito</button>
+            <button id="btn-encargar">Encargar</button>
+        </div>`;
     }
     contenedorCarrito.innerHTML = html;
+
+    if(hayProductos){
+        contenedorCarrito.addEventListener("click", (e) => {
+            if (e.target && e.target.id === "btn-delete") {
+                mostrarEliminarProducto();
+            }
+            if (e.target && e.target.id === "btn-encargar") {
+                mostrarFormularioEncargue();
+            }
+        });
+    }
+    
+
+    
 }
 
+    function mostrarFormularioEncargue(){
+        let encargarHTML = `
+        <div class="formEncargue">
+            <label for="">Nombre</label>
+            <input type="text">
+            <label for="">Apellido</label>
+            <input type="text">
+            <label for="">Telefono</label>
+            <input type="number">
+            <label for="">Dirección</label>
+            <input type="text">
+            <button id="finalizarCompra">Finalizar compra</button>
+        </div>`;
+        contenedorCarrito.innerHTML = encargarHTML;
 
+        document.getElementById("finalizarCompra").addEventListener("click", ()=>{
+            Swal.fire({
+                title: "GRACIAS POR CONFIAR EN NOSOTROS!",
+                text: "En las proximas horas nos comunicaremos con usted para realizar el envio de los productos.",
+                icon: "success"
+            });
+            carrito = carrito.map(producto => ({ ...producto, cantidad: 0 }));
+        guardarCarritoEnLocalStorage();
+        mostrarCarrito(carrito); 
+    });
+}
+
+    function mostrarEliminarProducto(){
+        let eliminarHtml = `
+                <div class="eliminar-producto">
+                    <label for="producto-eliminar">¿Qué producto necesita quitar del carrito?</label>
+                    <select id="producto-eliminar">
+                        ${catalogo.map((producto, index) => 
+                            `<option value="${index}">${producto.nombre} (${producto.peso})</option>`
+                        ).join('')}
+                    </select>
+                    <label for="cantidad-eliminar">Ingrese la cantidad que desea eliminar:</label>
+                    <input type="number" id="cantidad-eliminar" min="1" value="1">
+                    <button id="btn-confirmar-eliminar">Confirmar eliminación</button>
+                </div>
+            `;
+            contenedorCarrito.innerHTML = eliminarHtml;
+
+            document.getElementById("btn-confirmar-eliminar").addEventListener("click", () => {
+                let productoSeleccionado = parseInt(document.getElementById("producto-eliminar").value);
+                let cantidadEliminar = parseInt(document.getElementById("cantidad-eliminar").value);
+
+                if (!isNaN(productoSeleccionado) && cantidadEliminar > 0) {
+                    if (cantidadEliminar >= carrito[productoSeleccionado].cantidad){
+                        carrito[productoSeleccionado].cantidad = 0;
+                    } else {
+                        carrito[productoSeleccionado].cantidad = restar(carrito[productoSeleccionado].cantidad, cantidadEliminar);
+                    }
+
+                    guardarCarritoEnLocalStorage();
+                    Swal.fire({
+                        title: "Producto eliminado",
+                        text: `Se eliminaron ${cantidadEliminar} unidades de ${carrito[productoSeleccionado].nombre}.`,
+                        icon: "success"
+                    });
+                    mostrarCarrito(carrito);
+                } else {
+                    Swal.fire({
+                        title: "Cantidad invalida",
+                        icon: "error"
+                    });
+                }
+            });
+    }
 
 
 function sumar(a,b){
@@ -80,6 +175,7 @@ function buscarPorPrecio(arr, filtro){
 }
 
 
+
         function crearHTML(arr){
             let html = "";
             arr.forEach((producto, index) => {
@@ -104,7 +200,7 @@ function buscarPorPrecio(arr, filtro){
             </div>`;
             });
             
-            contenedor.innerHTML += html;
+            contenedor.innerHTML = html;
 
             document.querySelectorAll(".btn-agregar").forEach(boton => {
                 boton.addEventListener("click", (e) => {
@@ -118,80 +214,48 @@ function buscarPorPrecio(arr, filtro){
                     let index = e.target.getAttribute("data-producto");
                     let inputCantidad = document.getElementById(`cantidad-${index}`);
                     let ingreso = parseInt(inputCantidad.value);
-                    if (ingreso && !isNaN(ingreso) && ingreso > 0) {
-                        carrito[`producto${parseInt(index) + 1}`].cantidad = sumar(carrito[`producto${parseInt(index) + 1}`].cantidad, ingreso);
-                        Swal.fire({
-                            title: "Producto Agregado",
-                            text: `Agregaste ${ingreso} unidades de ${carrito[`producto${parseInt(index) + 1}`].nombre} al carrito.`,
-                            icon: "success"
-                        });
-                        document.getElementById(`cantidad-container-${index}`).style.display = "none";
+                    if (carrito[index]) {
+                        if (ingreso && !isNaN(ingreso) && ingreso > 0) {
+                            carrito[index].cantidad = sumar(carrito[index].cantidad, ingreso);
+                            guardarCarritoEnLocalStorage();
+                            Swal.fire({
+                                title: "Producto Agregado",
+                                text: `Agregaste ${ingreso} unidades de ${carrito[index].nombre} al carrito.`,
+                                icon: "success"
+                            });
+                            document.getElementById(`cantidad-container-${index}`).style.display = "none";
+                        } else {
+                            Swal.fire({
+                                title: "Cantidad invalida",
+                                icon: "error"
+                            });
+                        }
                     } else {
                         Swal.fire({
-                            title: "Cantidad invalida",
+                            title: "Error",
+                            text: "Producto no encontrado.",
                             icon: "error"
                         });
                     }
                 });
             });
         }
-        const borrarProducto = document.getElementById("btn-delete");
 
-borrarProducto.addEventListener("click", () => {
-    let eliminarHtml = `
-        <div class="eliminar-producto">
-            <label for="producto-eliminar">¿Qué producto necesita quitar del carrito?</label>
-            <select id="producto-eliminar">
-                <option value="1">Leña (6kg)</option>
-                <option value="2">Carbón</option>
-                <option value="3">Leña (15kg)</option>
-                <option value="4">Leña suelta</option>
-            </select>
-            <label for="cantidad-eliminar">Ingrese la cantidad que desea eliminar:</label>
-            <input type="number" id="cantidad-eliminar" min="1" value="1">
-            <button id="btn-confirmar-eliminar">Confirmar eliminación</button>
-        </div>
-    `;
-    contenedor.innerHTML = eliminarHtml;
+    function guardarCarritoEnLocalStorage(){
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    }
+    
+    function cargarCarritoDesdeLocalStorage(){
+        const carritoGuardado = localStorage.getItem("carrito");
+        if(carritoGuardado){
+            let carritoCargado = JSON.parse(carritoGuardado);
 
-    document.getElementById("btn-confirmar-eliminar").addEventListener("click", () => {
-        let productoSeleccionado = document.getElementById("producto-eliminar").value;
-        let cantidadEliminar = parseInt(document.getElementById("cantidad-eliminar").value);
-
-        if (productoSeleccionado && cantidadEliminar && !isNaN(cantidadEliminar)) {
-            let productoKey = `producto${productoSeleccionado}`;
-            if (carrito[productoKey]) {
-                if (cantidadEliminar >= carrito[productoKey].cantidad) {
-                    carrito[productoKey].cantidad = 0;
-                    Swal.fire({
-                        title: "Producto eliminado",
-                        text: `Eliminaste ${ingreso} unidades de ${carrito[`producto${parseInt(index) + 1}`].nombre} al carrito.`,
-                        icon: "success"
-                    });
-                } else {
-                    carrito[productoKey].cantidad = restar(carrito[productoKey].cantidad, cantidadEliminar);
-                    Swal.fire({
-                        title: "Producto Eliminado",
-                        text: `Se eliminaron ${cantidadEliminar} unidades de ${carrito[productoKey].nombre}.`,
-                        icon: "success"
-                    });
-                }
+            if (carritoCargado.length === catalogo.length) {
+                carrito = carritoCargado;
             } else {
-                Swal.fire({
-                    title: "El producto seleccionado no es válido",
-                    icon: "error"
-                });
+                carrito = catalogo.map(producto => ({ ...producto, cantidad: 0 }));
             }
-        } else {
-            Swal.fire({
-                title: "Cantidad invalida",
-                icon: "error"
-            });
         }
-        document.querySelector('.eliminar-producto').remove();
-        crearHTML(catalogo);
-    });
-});
-
-
-    crearHTML(catalogo);
+    }
+    
+    window.addEventListener("load", cargarCarritoDesdeLocalStorage);
