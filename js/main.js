f110bc7e6eda69f6aa753ef3624b363e2bd97402
Agustin-Sosa <agustinomarsosa@gmail.com>
2024-09-22 19:07:01 -0300

    proyecto final terminado

--- js/main.js
+++ js/main.js
@@ -10,9 +10,12 @@ fetch('./data/productos.json')
     })
     .then((data) =>{
         catalogo = data;
-        carrito = catalogo.map(producto => ({ ...producto, cantidad: 0 }));
-        crearHTML(catalogo)
         cargarCarritoDesdeLocalStorage();
+        if (!carrito.length || carrito.length !== catalogo.length) {
+            carrito = catalogo.map(producto => ({ ...producto, cantidad: 0 }));
+        }
+        crearHTML(catalogo);
+        guardarCarritoEnLocalStorage();
     })
     .catch((error) => {
         console.error("Hubo un problema con el fetch:", error);
@@ -28,6 +31,7 @@ const verCarrito = document.getElementById("btn-carrito");
 
 let carritoVisible = false;
 
+
 verCarrito.addEventListener('click', () => {
     if (carritoVisible) {
         contenedorCarrito.innerHTML = "";
@@ -40,15 +44,24 @@ verCarrito.addEventListener('click', () => {
     }
 });
 
+contenedorCarrito.addEventListener("click", (e) => {
+    if (e.target && e.target.id === "btn-delete") {
+        mostrarEliminarProducto();
+    }
+    if (e.target && e.target.id === "btn-encargar") {
+        mostrarFormularioEncargue();
+    }
+});
+
 function mostrarCarrito(obj) {
-    let html = '';
+    let html = "";
     let hayProductos = false;
     for (const key in obj) {
         const producto = obj[key];
         if(producto.cantidad > 0){
             hayProductos = true;
             html += 
-        `<div class="">
+        `<div class="prod-carrito">
             <h2>Cantidad de ${producto.nombre} (${producto.peso}): ${producto.cantidad}</h2>
         </div>`;
         }
@@ -57,52 +70,52 @@ function mostrarCarrito(obj) {
         html = `<div><h2>El carrito está vacío</h2></div>`;
     } else {
         html += 
-        `<div>
-            <button id="btn-delete">Eliminar producto del carrito</button>
-            <button id="btn-encargar">Encargar</button>
+        `<div class="botonesCarrito">
+            <button id="btn-delete" class="btn-main">Eliminar producto del carrito</button>
+            <button id="btn-encargar" class="btn-main">Encargar</button>
         </div>`;
     }
     contenedorCarrito.innerHTML = html;
 
-    if(hayProductos){
-        contenedorCarrito.addEventListener("click", (e) => {
-            if (e.target && e.target.id === "btn-delete") {
-                mostrarEliminarProducto();
-            }
-            if (e.target && e.target.id === "btn-encargar") {
-                mostrarFormularioEncargue();
-            }
-        });
-    }
-    
-
-    
 }
 
     function mostrarFormularioEncargue(){
+        
         let encargarHTML = `
         <div class="formEncargue">
             <label for="">Nombre</label>
-            <input type="text">
+            <input type="text" id="nombre">
             <label for="">Apellido</label>
-            <input type="text">
+            <input type="text" id="apellido">
             <label for="">Telefono</label>
-            <input type="number">
+            <input type="number" id="telefono">
             <label for="">Dirección</label>
-            <input type="text">
-            <button id="finalizarCompra">Finalizar compra</button>
+            <input type="text" id="direccion">
+            <button id="finalizarCompra" class="btn-main">Finalizar compra</button>
         </div>`;
-        contenedorCarrito.innerHTML = encargarHTML;
+        contenedorCarrito.innerHTML += encargarHTML;
 
         document.getElementById("finalizarCompra").addEventListener("click", ()=>{
+            let nombre = document.getElementById("nombre").value;
+            let apellido = document.getElementById("apellido").value;
+            let telefono = document.getElementById("telefono").value;
+            let direccion = document.getElementById("direccion").value;
+            if (nombre === "" || apellido === "" || telefono === "" || direccion === "") {
+            Swal.fire({
+                title: "Error",
+                text: "Por favor, complete todos los campos.",
+                icon: "error"
+            });
+        } else {
             Swal.fire({
                 title: "GRACIAS POR CONFIAR EN NOSOTROS!",
-                text: "En las proximas horas nos comunicaremos con usted para realizar el envio de los productos.",
+                text: "En unos minutos nos comunicaremos con usted para realizar el envio de los productos.",
                 icon: "success"
             });
             carrito = carrito.map(producto => ({ ...producto, cantidad: 0 }));
         guardarCarritoEnLocalStorage();
         mostrarCarrito(carrito); 
+    }
     });
 }
 
@@ -117,10 +130,10 @@ function mostrarCarrito(obj) {
                     </select>
                     <label for="cantidad-eliminar">Ingrese la cantidad que desea eliminar:</label>
                     <input type="number" id="cantidad-eliminar" min="1" value="1">
-                    <button id="btn-confirmar-eliminar">Confirmar eliminación</button>
+                    <button class="btn-main" id="btn-confirmar-eliminar">Confirmar eliminación</button>
                 </div>
             `;
-            contenedorCarrito.innerHTML = eliminarHtml;
+            contenedorCarrito.innerHTML += eliminarHtml;
 
             document.getElementById("btn-confirmar-eliminar").addEventListener("click", () => {
                 let productoSeleccionado = parseInt(document.getElementById("producto-eliminar").value);
@@ -186,16 +199,22 @@ function buscarPorPrecio(arr, filtro){
             </div>
             <div class="producto-info">
                 <h2>${producto.nombre}</h2>
-                <h4>${producto.peso}</h4>
-                <h4>Precio: $ ${producto.precio}</h4>
+                <h3>
+                    <img src="./assets/img/balanza.png">
+                    ${producto.peso}
+                </h3>
+                <h3>
+                <img src="./assets/img/etiqueta-de-precio.png">
+                Precio: $ ${producto.precio}
+                </h3>
                 <div class="producto-cantidad" id="cantidad-container-${index}" style="display: none;">
                     <label for="cantidad-${index}">Cantidad:</label>
                     <input type="number" id="cantidad-${index}" min="1" value="1">
-                    <button class="btn-confirmar" data-producto="${index}">Confirmar</button>
+                    <button class="btn-confirmar btn-main" data-producto="${index}">Confirmar</button>
                 </div>
             </div>
             <div class="producto-agregar">
-                <button class="btn-agregar" data-producto="${index}">Agregar al carrito</button>
+                <button class="btn-agregar btn-main" data-producto="${index}">Agregar al carrito</button>
             </div>
             </div>`;
             });
@@ -249,11 +268,8 @@ function buscarPorPrecio(arr, filtro){
         const carritoGuardado = localStorage.getItem("carrito");
         if(carritoGuardado){
             let carritoCargado = JSON.parse(carritoGuardado);
-
             if (carritoCargado.length === catalogo.length) {
                 carrito = carritoCargado;
-            } else {
-                carrito = catalogo.map(producto => ({ ...producto, cantidad: 0 }));
             }
         }
     }
